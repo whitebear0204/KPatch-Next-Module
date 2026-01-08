ef136baead5d38f15fb545839e5f2b9c11f0ae8d
KOWX712 <leecc0503@gmail.com>
2026-01-09 00:32:01 +0800

    webui/patch: disallow invalid character
    ref: bmax121/APatch@93154859b69e588f413adff397a80e7c45598987
    
    #1

--- webui/index.js
+++ webui/index.js
@@ -61,8 +61,14 @@ function updateSuperkey(key) {
 }
 
 function updateBtnState(value) {
+    const isInvalid = /[$"'[\]`]/.test(value);
+    const field = document.querySelector('#superkey .password-field');
+
+    field.error = isInvalid;
+    field.errorText = isInvalid ? getString('msg_invalid_char') : '';
+
     document.querySelector('#superkey-dialog .confirm').disabled = !value;
-    document.getElementById('start').disabled = !value;
+    document.getElementById('start').disabled = !value || isInvalid;
 }
 
 export async function initInfo() {
